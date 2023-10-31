b3b40ad078ba152691d80a64cd90b59d347d58d2
Olle Ristenstrand <olelwow@hotmail.com>
2023-10-31 18:09:08 +0100

    Dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,30 @@ const body  = document.body;
 const content = document.getElementsByClassName("omMig");
 const head = document.getElementsByClassName("huvud");
 const foot = document.getElementsByClassName("fot");
+const menyContents = head[0].childNodes[1];
+const menyText = menyContents.childNodes[3].childNodes[1];
+const facebook = foot[0].childNodes[1].children[0];
+const instagram = foot[0].childNodes[1].children[1];
+const linkedIn = foot[0].childNodes[1].children[2];
 
-const paragraf = content[0].childNodes;
+
 function toggleDark () {
     
     body.classList.toggle("dark");
-    console.log(body.classList);
-    console.log(content);
-    console.log(paragraf);
-    console.log(head);
+    
+     console.log(content[0]);
+     console.log(head);
+     console.log(menyContents.childNodes[3])
+     console.log(menyText);
+     console.log(facebook);
+    content[0].classList.toggle("darkParagraf");
     head[0].classList.toggle("darkHead");
-    head[0].firstElementChild.classList.toggle("darkNav");
-    head[0].lastElementChild.classList.toggle("darkMeny");
+    foot[0].classList.toggle("darkFoot");
+    facebook.classList.toggle("darkFb");
+    instagram.classList.toggle("darkInsta");
+    linkedIn.classList.toggle("darkLinkedIn");
+  
+    
 
-
-    
-    
-    
-    
-    
-    
     
 }
