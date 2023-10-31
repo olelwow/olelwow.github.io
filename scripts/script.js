c0e70b68a60dd455ae3f53946dfa8cc010ecce02
Olle Ristenstrand <olelwow@hotmail.com>
2023-10-31 19:15:49 +0100

    Gjort skit med skiten

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,31 @@ const body  = document.body;
 const content = document.getElementsByClassName("omMig");
 const head = document.getElementsByClassName("huvud");
 const foot = document.getElementsByClassName("fot");
-const menyContents = head[0].childNodes[1];
-const menyText = menyContents.childNodes[3].childNodes[1];
+
+const menyContents = head[0].childNodes[3];
+const menyText1 = menyContents.childNodes[1].childNodes[1].childNodes[1];
+const menyText2 = menyContents.childNodes[1].childNodes[3].childNodes[1];
+const menyText3 = menyContents.childNodes[1].childNodes[5].childNodes[1];
 const facebook = foot[0].childNodes[1].children[0];
 const instagram = foot[0].childNodes[1].children[1];
 const linkedIn = foot[0].childNodes[1].children[2];
 
 
-function toggleDark () {
+function toggleDarkModeIndex () {
+    
+    
+    console.log(menyText1);
     
     body.classList.toggle("dark");
-    
-     console.log(content[0]);
-     console.log(head);
-     console.log(menyContents.childNodes[3])
-     console.log(menyText);
-     console.log(facebook);
     content[0].classList.toggle("darkParagraf");
     head[0].classList.toggle("darkHead");
     foot[0].classList.toggle("darkFoot");
     facebook.classList.toggle("darkFb");
     instagram.classList.toggle("darkInsta");
     linkedIn.classList.toggle("darkLinkedIn");
-  
-    
-
+    menyText1.classList.toggle("darkModeText");
+    menyText2.classList.toggle("darkModeText");
+    menyText3.classList.toggle("darkModeText");
     
 }
+
