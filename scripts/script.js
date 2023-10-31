b40f61f32763cf1e6a0d9b6f401ce6b9deedb00d
Olle Ristenstrand <olelwow@hotmail.com>
2023-10-31 13:33:36 +0100

    darkmode development

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
-function hallåDöh () {
-    document.getElementById("kattParagraf").style.display = 'block';
+const body  = document.body;
+const content = document.getElementsByClassName("omMig");
+const head = document.getElementsByClassName("huvud");
+const foot = document.getElementsByClassName("fot");
+
+const paragraf = content[0].childNodes;
+function toggleDark () {
+    
+    body.classList.toggle("dark");
+    console.log(body.classList);
+    console.log(content);
+    console.log(paragraf);
+    console.log(head);
+    head[0].classList.toggle("darkHead");
+    head[0].firstElementChild.classList.toggle("darkNav");
+    head[0].lastElementChild.classList.toggle("darkMeny");
+
+
+    
+    
+    
+    
+    
+    
+    
 }
