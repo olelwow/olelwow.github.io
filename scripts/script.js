33faa6d2eeef1287ef4bcdfc9430c6ee7857d25c
Olle Ristenstrand <olelwow@hotmail.com>
2023-11-03 13:10:39 +0100

    Kattttt jävel

--- scripts/script.js
+++ scripts/script.js
@@ -11,36 +11,88 @@
 // const instagram = foot[0].childNodes[1].children[1];
 // const linkedIn = foot[0].childNodes[1].children[2];
 
+// const darkModeToggle = document.getElementById("darkModeToggle");
+// let darkCookie = document.cookie = "isDark=false; SameSite=Lax;";
+
+// function toggleDarkMode () {
+    
+//     if (document.cookie.endsWith("false")) {
+//     darkModeToggle.href = "./css/darkStyle.css";
+//     darkCookie = document.cookie = "isDark=true; SameSite=Lax;";
+//     }
+//     else if (document.cookie.endsWith("true")) {
+//         darkModeToggle.href = "./css/style.css";
+//         darkCookie = document.cookie = "isDark=false; SameSite=Lax";
+//     }
+    
+    
+// }
+
 const darkModeToggle = document.getElementById("darkModeToggle");
-let darkCookie = document.cookie = "isDark=false; SameSite=Lax;";
+const isDark = getCookie("isDark");
 
-
-
-
-
-
-
-function toggleDarkMode () {
-    
-    if (document.cookie.endsWith("false")) {
+// Check if the 'isDark' cookie is set and toggle dark mode accordingly
+if (isDark === "true") {
     darkModeToggle.href = "./css/darkStyle.css";
-    darkCookie = document.cookie = "isDark=true; SameSite=Lax;";
-    }
-    else if (document.cookie.endsWith("true")) {
-        darkModeToggle.href = "./css/style.css";
-        darkCookie = document.cookie = "isDark=false; SameSite=Lax";
-    }
-    console.log(darkCookie);
-    // body.classList.toggle("dark");
-    // content[0].classList.toggle("darkParagraf");
-    // head[0].classList.toggle("darkHead");
-    // foot[0].classList.toggle("darkFoot");
-    // facebook.classList.toggle("darkFb");
-    // instagram.classList.toggle("darkInsta");
-    // linkedIn.classList.toggle("darkLinkedIn");
-    // menyText1.classList.toggle("darkModeText");
-    // menyText2.classList.toggle("darkModeText");
-    // menyText3.classList.toggle("darkModeText");
-    
+} else {
+    darkModeToggle.href = "./css/style.css";
 }
 
+function toggleDarkMode() {
+    if (isDark === "true") {
+        darkModeToggle.href = "./css/style.css";
+        setCookie("isDark", "false");
+    } else {
+        darkModeToggle.href = "./css/darkStyle.css";
+        setCookie("isDark", "true");
+    }
+}
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + value + "; SameSite=Lax; path=/";
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + "=")) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return "";
+}
+
+function updateClock () {
+    const date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    hours = hours < 10 ? "0" + hours: hours;
+    minutes = minutes < 10 ? "0" + minutes: minutes;
+    seconds = seconds < 10 ? "0" + seconds: seconds;
+
+    let currentTime = 
+              hours
+              + ":"
+              + minutes
+              + ":" 
+              + seconds;
+
+    document.getElementById("clock").innerHTML = currentTime;          
+}
+
+function updateDate () {
+    const date = new Date();
+
+    document.getElementById("date").innerHTML = date.toDateString();
+
+
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+updateDate();
+setInterval(updateDate, 100000);
